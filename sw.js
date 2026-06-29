4c012eecd1d5965d9cea5ce7c69f5b3a9d3713e5
PTHSAMA <106174782+PTHSAMA@users.noreply.github.com>
2026-06-29 13:57:36 +0900

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "yezhu-taxfree-v4";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const cloned = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          if (event.request.method === "GET") {
+            cache.put(event.request, cloned);
+          }
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
